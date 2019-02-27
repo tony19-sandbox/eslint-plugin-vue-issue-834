9c2408328742b7835c831de01cdfc27da7bfce19
Tony Trinh <tony19@gmail.com>
2019-02-26 19:23:53 -0600

    Add vue/script-indent rule to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,21 @@ module.exports = {
     '@vue/standard',
     '@vue/typescript'
   ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/script-indent': [
+      'error',
+      2,
+      {
+        'baseIndent': 1,
+        'ignores': [
+          '[value.type="ObjectExpression"]:not(:matches(ExportDefaultDeclaration, [left.property.name="exports"]) > * > [value.type="ObjectExpression"])',
+          '[value.type="ArrayExpression"]'
+        ]
+      }
+    ]
+  },
   parserOptions: {
     parser: '@typescript-eslint/parser'
   }
